0b91e907231d2abd265b1f2174b38b1687f307c7
hbadshah <honeybadsah09@gmail.com>
2024-05-15 16:48:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+ // App.js 
+
+import './App.css'; 
+import Keyboard from './components/keyboard'; 
+
+function App() { 
+return ( 
+	<div className="App"> 
+	<Keyboard/> 
+	</div> 
+); 
+} 
+
+export default App; 
